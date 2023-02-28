b1ea57596f62122c5569c5c8d4cb8c7f38670175
Kirill Sakharov <kirillsakharov@dyn3207-119.wlan.ic.ac.uk>
2023-02-28 13:16:30 +0000

    changed scene

--- client/src/mainComp/mainScene.js
+++ client/src/mainComp/mainScene.js
@@ -1,17 +1,34 @@
-var Scene = require("@babylonjs/core").Scene;
-var Engine = require("@babylonjs/core").Engine;
-var Vector3 = require("@babylonjs/core").Vector3;
-var HemisphericLight = require("@babylonjs/core").HemisphericLight;
-var ArcRotateCamera = require("@babylonjs/core").ArcRotateCamera;
-// var StandardMaterial = require("@babylonjs/core").StandardMaterial;
-var CubeTexture = require("@babylonjs/core").CubeTexture;
-var Ray = require("@babylonjs/core").Ray;
-//var RayHelper = require("@babylonjs/core").RayHelper;
-//var WebXRDefaultExperience = require("@babylonjs/core").WebXRDefaultExperience;
-var EnvironmentHelper = require("@babylonjs/core").EnvironmentHelper;
-// var MeshBuilder = require("@babylonjs/core").MeshBuilder;
-// var Color3 = require("@babylonjs/core").Color3;
-var SceneLoader = require("@babylonjs/core").SceneLoader;
+import {
+  ShadowGenerator,
+  GizmoManager,
+  LightGizmo,
+  TransformNode,
+  VertexData,
+  /*VertexBuffer,*/ Mesh,
+  Scene,
+  Engine,
+  /*Color3,*/ Vector3,
+  /*MeshBuilder,*/ HemisphericLight,
+  EnvironmentHelper,
+  WebXRDefaultExperience,
+  ArcRotateCamera,
+  /*StandardMaterial,*/ CubeTexture,
+  Ray,
+  RayHelper,
+  SceneLoader,
+  PBRMaterial,
+  Texture,
+  SpotLight,
+  DirectionalLight,
+} from "@babylonjs/core";
+import {
+  NearMenu,
+  TouchHolographicButton,
+  /*ColorPicker, TextBlock, StackPanel, AdvancedDynamicTexture, Control,*/ HolographicButton,
+  CylinderPanel,
+  GUI3DManager /*panel, anchor MeshButton3D */,
+} from "@babylonjs/gui";
+
 var fileInput = document.getElementById("loadFile");
 if (!fileInput) {
   fileInput = document.createElement("INPUT");
@@ -25,51 +42,37 @@ if (!fileInput) {
   document.body.children[0].appendChild(fileInput);
 }
 
-require("@babylonjs/core/Materials/Textures/Loaders");
-require("@babylonjs/core/Materials/Node/Blocks");
-require("@babylonjs/loaders");
+import "@babylonjs/core/Materials/Textures/Loaders";
+import "@babylonjs/core/Materials/Node/Blocks";
+import "@babylonjs/loaders";
 
 export class mainScene {
   scene;
   engine;
 
   constructor(canvas) {
-    this.engine = new Engine(canvas, true);
+    this.canvas = canvas;
+    this.engine = new Engine(this.canvas, true);
     this.scene = this.CreateScene();
 
-    /* SceneLoader.ImportMesh(
-      "",
-      "./models/",
-      "item.glb",
-      this.scene,
-      function (meshes) {
-        console.log("meshes", meshes);
-      }
-    );*/
-    /*
-    SceneLoader.ImportMesh(
-      "",
-      "https://realviewtest1.s3.eu-west-2.amazonaws.com/models/",
-      "item.glb",
-      this.scene,
-      function (newMeshes) {
-        var importedMesh = newMeshes[0];
-        console.log(importedMesh);
-        // do something with the imported mesh
-      }
-    );*/
+    //const manager = new GUI.GUI3DManager(this.scene);
 
-    const result = SceneLoader.ImportMesh("", "./models/", "item.glb");
-    const meshes = result.meshes;
+    // panel.blockLayout = true;
+    // for (let index = 0; index < 30; index++) {
+    //   const button = new GUI.Button3D("click me");
+    //   manager.addControl(panel);
+    //   panel.addControl(button);
+    // }
+    // panel.blockLayout = false;
 
-    console.log("meshes", meshes);
+    // var manager = new GUI.GUI3DManager(this.scene);
 
-    const hemiLight = new HemisphericLight(
-      "hemiLight",
-      new Vector3(0, 1, 0),
-      this.scene
-    );
-    hemiLight.intensity = 0.5;
+    // var panel = new GUI.CylinderPanel();
+    // panel.margin = 0.2;
+
+    // manager.addControl(panel);
+    // panel.linkToTransformNode(anchor);
+    // panel.position.z = 0;
 
     const camera = new ArcRotateCamera(
       "Camera",
@@ -90,33 +93,166 @@ export class mainScene {
 
   CreateScene() {
     const scene = new Scene(this.engine);
-
     return scene;
   }
 
   async initXR() {
-    const envTex = CubeTexture.CreateFromPrefilteredData(
-      "./environment/environment.env",
-      this.scene
-    );
     const envHelper = new EnvironmentHelper(
       {
-        groundOpacity: 0.75,
+        groundOpacity: 3,
       },
       this.scene
     );
 
-    envHelper.skybox?.dispose();
-    envHelper.ground.scaling = new Vector3(5, 5, 5);
-    envHelper.ground.position.y = -1.1;
-    envHelper.ground.isPickable = false;
+    const hemiLight = new HemisphericLight(
+      "hemiLight",
+      new Vector3(0, 1, 0),
+      this.scene
+    );
+
+    hemiLight.intensity = 0;
+
+    const envTex = CubeTexture.CreateFromPrefilteredData(
+      "./environment/sky.env",
+      this.scene
+    );
+    this.scene.environmentTexture = envTex;
+    this.scene.createDefaultSkybox(envTex, true);
+
+    SceneLoader.ImportMeshAsync("", "./models/", "white-room1.glb").then(
+      (result2) => {
+        result2.meshes.forEach((mesh) => {
+          mesh.scaling = new Vector3(2, 2, 2);
+          mesh.position.y = -0.7;
+          mesh.isPickable = false;
+        });
+      }
+    );
+
+    // // Import radial GUI from Dropbox.
+    // var radialGuiImportResult = await SceneLoader.ImportMeshAsync("", "https://dl.dropbox.com/s/5gw2ibz1k6p6r17/radialGuiWithIcons.glb", "radialGuiWithIcons.glb", this.scene);
+    // // Create radial GUI transform node.
+    // var radialGui = new TransformNode("radialGui")
+    // radialGuiImportResult.meshes.forEach((mesh, index) => {
+    //   if (index > 0) mesh.setParent(radialGui)
+    // })
+    // // dispose "__root__" after handling/
+    // radialGuiImportResult.meshes[0].dispose();
+
+    // Transform meshes into buttons.
+    var manager = new GUI3DManager(this.scene);
+    // manager.useRealisticScaling = true;
+
+    const near = new NearMenu("near");
+    manager.addControl(near);
+    let follower = near.defaultBehavior.followBehavior; //returns the followbehavior created by the
+    follower.defaultDistance = 3;
+    follower.minimumDistance = 1;
+    follower.maximumDistance = 5;
+
+    const button0 = new TouchHolographicButton("button0");
+    // button0.imageUrl = "./textures/IconFollowMe.png";
+    button0.text = "Button 0";
+    near.addButton(button0);
+
+    const button1 = new TouchHolographicButton("button1");
+    // button1.imageUrl = "./textures/IconClose.png";
+    button1.text = "Button 1";
+    near.addButton(button1);
+
+    const button2 = new TouchHolographicButton("button2");
+    // button2.imageUrl = "./textures/IconFollowMe.png";
+    button2.text = "Button 2";
+    near.addButton(button2);
+
+    near.isPinned = true;
+
+    // // Handle buttons.
+    // radialGuiImportResult.meshes.forEach((slice) => {
+    //   // Apply button material only to buttons.
+    //   if (!slice.name.includes("button")) return
+    //   // Default colour/material.
+    //   var highlightMaterial = new StandardMaterial("highlight", this.scene);
+    //   highlightMaterial.diffuseColor = new Color3(0.42, 0.4, 0.38);
+    //   highlightMaterial.backFaceCulling = false
+    //   highlightMaterial.useLightmapAsShadowmap = true;
+    //   slice.material = highlightMaterial;
+    //   // Create push button.
+    //   var pushButton = new MeshButton3D(slice, "pushButton");
+    //   // Add button animations.
+    //   pushButton.pointerEnterAnimation = () => {
+    //     slice.material.diffuseColor = new Color3(0, 0.02, 1);
+    //   };
+    //   pushButton.pointerOutAnimation = () => {
+    //     slice.material.diffuseColor = new Color3(0.42, 0.4, 0.38);
+    //   };
+    //   // Add button to manager.
+    //   manager.addControl(pushButton)
+    // })
+
+    var anchor = new TransformNode("");
+
+    var panel = new CylinderPanel();
+    panel.margin = 0.2;
+
+    manager.addControl(panel);
+    panel.linkToTransformNode(anchor);
+    panel.position.z = -0.5;
+    panel.position.y = 1.75;
+
+    // Let's add some buttons!
+    var addButton = function () {
+      var button = new HolographicButton("orientation");
+      panel.addControl(button);
+
+      button.text = "Button #" + panel.children.length;
+    };
+
+    panel.blockLayout = true;
+    for (var index = 0; index < 30; index++) {
+      addButton();
+    }
+    panel.blockLayout = false;
+
+    const spotLight = new SpotLight(
+      "spotLight",
+      new Vector3(0, 0.5, -3),
+      new Vector3(0, 1, 3),
+      Math.PI / 2,
+      10,
+      this.scene
+    );
+    spotLight.intensity = 100;
+    spotLight.shadowEnabled = true;
+    const shadowGen = new ShadowGenerator(2048, spotLight);
+
+    const lightGizmo = new LightGizmo();
+    lightGizmo.scaleRatio = 4;
+    lightGizmo.light = spotLight;
+
+    const gizmoManager = new GizmoManager(this.scene);
+    gizmoManager.positionGizmoEnabled = true;
+    gizmoManager.rotationGizmoEnabled = true;
+    gizmoManager.usePointerToAttachGizmos = false;
+    gizmoManager.attachToMesh(lightGizmo.attachedMesh);
+
+    const directionalLight = new DirectionalLight(
+      "directionalLight",
+      new Vector3(-10, -5, -8.5),
+      this.scene
+    );
+    directionalLight.shadowEnabled = true;
+    directionalLight.position = new Vector3(100, 50, 85);
+    const shadowGen2 = new ShadowGenerator(2048, directionalLight);
+
+    // const lightGizmo2 = new LightGizmo();
+    // lightGizmo2.scaleRatio = 2;
+    // lightGizmo2.light = directionalLight;
+
+    // gizmoManager.attachToMesh(lightGizmo2.attachedMesh);
 
     // const sphereD = 1.0;
-    // const sphere = MeshBuilder.CreateSphere(
-    //   "Sphere",
-    //   { segments: 16, diameter: sphereD },
-    //   this.scene
-    // );
+    // const sphere = MeshBuilder.CreateSphere('Sphere', { segments: 16, diameter: sphereD }, this.scene);
     // sphere.position.x = 0;
     // sphere.position.y = sphereD * 2;
     // sphere.position.z = 0;
@@ -126,43 +262,165 @@ export class mainScene {
     // rMat.diffuseColor = new Color3(1.0, 0, 0);
     // sphere.material = rMat;
 
-    this.scene.createDefaultSkybox(envTex, true);
+    // shadowGen.getShadowMap().renderList.push(sphere);
+    // shadowGen.addShadowCaster(sphere);
 
-    //this.scene.environmentIntensity = 0.5;
+    // var plane = MeshBuilder.CreatePlane("plane");
+    // plane.position = new Vector3(1.4, 1.5, 0.4)
+    // var advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
+    // var panel2 = new StackPanel();
+    // advancedTexture.addControl(panel2);
+    // var header = new TextBlock();
+    // header.text = "Color GUI";
+    // header.height = "100px";
+    // header.color = "white";
+    // header.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+    // header.fontSize = "120"
+    // panel2.addControl(header);
+    // var picker = new ColorPicker();
+    // picker.value = sphere.material.diffuseColor;
+    // picker.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+    // picker.height = "350px";
+    // picker.width = "350px";
+    // picker.onValueChangedObservable.add(function (value) {
+    //   sphere.material.diffuseColor.copyFrom(value);
+    // });
+    // panel2.addControl(picker);
 
-    // Check XR support
-    const xr = await this.scene.createDefaultXRExperienceAsync({
-      floorMeshes: [envHelper.ground],
-    });
-    if (
-      !(await xr.baseExperience.sessionManager.supportsSessionAsync(
-        "immersive-vr"
-      ))
-    ) {
-      // do WebVR instead
-      const vrHelper = this.scene.createDefaultVRExperience();
-      vrHelper.enableInteractions();
-    }
+    envHelper.skybox?.dispose();
+    envHelper.ground.position.y = -0.2;
+    envHelper.ground.isPickable = false;
+    envHelper.ground.visibility = true;
+    //envHelper.ground.scaling = new Vector3(, 2.1, 2.1);
 
-    /*
+    const pbr = new PBRMaterial("pbr", this.scene);
+    pbr.albedoTexture = new Texture(
+      "./textures/mud/brown_mud_diffuse.jpeg",
+      this.scene
+    );
+    pbr.bumpTexture = new Texture(
+      "./textures/mud/brown_mud_normal.jpeg",
+      this.scene
+    );
+    pbr.invertNormalMapX = true;
+    pbr.invertNormalMapY = true;
+
+    pbr.useAmbientOcclusionFromMetallicTextureRed = true;
+
+    pbr.useRoughnessFromMetallicTextureGreen = true;
+
+    pbr.useMetallnessFromMetallicTextureBlue = true;
+
+    pbr.roughness = 0.9;
+    envHelper.ground.material = pbr;
+
+    //const skybox = (this.scene).createDefaultSkybox(envTex, true);
+
+    this.scene.environmentIntensity = 0.1;
+
     const xr = await WebXRDefaultExperience.CreateAsync(this.scene, {
       floorMeshes: [envHelper.ground],
       optionalFeatures: true,
-      outputCanvas: this.scene.getEngine().getRenderingCanvas(),
-    });*/
+    });
+
+    // let parentMesh;
+    SceneLoader.ImportMeshAsync("", "./models/", "Barrel_01.glb").then(
+      (result) => {
+        console.log("result.meshes", result.meshes);
+        // result.meshes[0].position.x = 5;
+        // result.meshes[1].isPickable = true;
+
+        //Create a parent mesh and set imported meshes as children
+        // parentMesh = new Mesh("parentMesh", this.scene);
+        // for (let i = 1; i < result.meshes.length; i++) {
+        //   result.meshes[i].position.x = 5;
+        //   result.meshes[i].isPickable = true;
+        //   result.meshes[i].setParent(parentMesh);
+        // }
+
+        var overrideMaterialSideOrientation = false;
+        for (var i = 0; i < result.meshes.length; i++) {
+          result.meshes[i].overrideMaterialSideOrientation =
+            overrideMaterialSideOrientation;
+        }
+
+        for (i = 1; i < result.meshes.length; i++) {
+          result.meshes[i].overrideMaterialSideOrientation =
+            overrideMaterialSideOrientation;
+          var vertexData = VertexData.ExtractFromMesh(result.meshes[i]);
+          var positions = vertexData.positions;
+
+          if (!positions) {
+            console.log("Mesh ", i, " is missing position data");
+          }
+
+          console.log(
+            "Positions for ",
+            i,
+            "which is ",
+            result.meshes[i].name,
+            "are: ",
+            positions
+          );
+        }
+
+        const mergedMesh = Mesh.MergeMeshes(
+          result.meshes.slice(1),
+          true,
+          true,
+          null,
+          false,
+          true
+        );
+        mergedMesh.isPickable = true;
+        mergedMesh.position = new Vector3(0, 0.3, 0);
+        mergedMesh.receiveShadows = true;
+
+        shadowGen.getShadowMap().renderList.push(mergedMesh);
+        shadowGen.addShadowCaster(mergedMesh);
+
+        shadowGen2.getShadowMap().renderList.push(mergedMesh);
+        shadowGen2.addShadowCaster(mergedMesh);
+
+        envHelper.ground.receiveShadows = true;
+
+        //shadowGen.addShadowCaster(mergedMesh);
+
+        // console.log("mergedMesh", mergedMesh.metadata);
+
+        // var positionsDefined = false;
+        // for (var i = 0; i < result.meshes.length; i++) {
+        //   if (result.meshes[i].geometry && result.meshes[i].getVerticesData(VertexBuffer.PositionKind)) {
+        //     positionsDefined = true;
+        //     break;
+        //   }
+        // }
+        // if (positionsDefined) {
+        //   var mergedMesh = Mesh.MergeMeshes(result.meshes, true);
+        //   mergedMesh.position.x = 10;
+        // } else {
+        //   console.log("Positions are not defined for one or more meshes");
+        // }
+
+        // console.log("meshes length", result.meshes.length);
+
+        // const mergedMesh = Mesh.MergeMeshes(result.meshes);
+        // console.log("merged.meshes", mergedMesh);
+        // console.log("mergedMesh", mergedMesh);
+      }
+    );
 
     const tmpRay = new Ray(new Vector3(), new Vector3(), 3);
-    const tmpRay2 = new Vector3(new Vector3(), new Vector3(), 3);
-    const tmpRay3 = new Vector3(new Vector3(), new Vector3(), 3);
+    const tmpRay2 = new Ray(new Vector3(), new Vector3(), 3);
+    const tmpRay3 = new Ray(new Vector3(), new Vector3(), 3);
 
-    // const rayHelper = new RayHelper(tmpRay);
-    // rayHelper.show(this.scene);
-    // const rayHelper2 = new RayHelper(tmpRay2);
-    // rayHelper2.show(this.scene);
-    // const rayHelper3 = new RayHelper(tmpRay3);
-    // rayHelper3.show(this.scene);
+    const rayHelper = new RayHelper(tmpRay);
+    rayHelper.show(this.scene);
+    const rayHelper2 = new RayHelper(tmpRay2);
+    rayHelper2.show(this.scene);
+    const rayHelper3 = new RayHelper(tmpRay3);
+    rayHelper3.show(this.scene);
 
-    let hit;
     let tmpMesh;
 
     xr.input.onControllerAddedObservable.add((controller) => {
@@ -174,7 +432,7 @@ export class mainScene {
             if (triggerComponent.value > 0.5) {
               controller.getWorldPointerRayToRef(tmpRay, false);
 
-              hit = this.scene.pickWithRay(tmpRay);
+              const hit = this.scene.pickWithRay(tmpRay);
 
               if (hit) {
                 if (hit.pickedMesh !== undefined) {
@@ -182,10 +440,17 @@ export class mainScene {
                     tmpMesh = hit.pickedMesh;
                     console.log("name:" + hit.pickedMesh.name);
                     tmpMesh.setParent(motionController.rootMesh);
+
+                    // tmpMesh = hit.pickedMesh;
+                    // console.log("name:" + hit.pickedMesh.name);
+                    // parentMesh.setParent(motionController.rootMesh);
                   }
                 }
               }
             } else if (triggerComponent.value < 0.5) {
+              // if (parentMesh.parent !== null) {
+              //   parentMesh.setParent(null);
+              // }
               if (tmpMesh != undefined) {
                 tmpMesh.setParent(null);
               }
@@ -197,14 +462,12 @@ export class mainScene {
             if (abuttonComponent.pressed) {
               controller.getWorldPointerRayToRef(tmpRay2, false);
 
-              hit = this.scene.pickWithRay(tmpRay2);
+              const hit = this.scene.pickWithRay(tmpRay2);
 
               if (hit && hit.pickedMesh) {
-                if (hit.pickedMesh) {
-                  hit.pickedMesh.scaling.x *= 0.9;
-                  hit.pickedMesh.scaling.y *= 0.9;
-                  hit.pickedMesh.scaling.z *= 0.9;
-                }
+                hit.pickedMesh.scaling.multiplyInPlace(
+                  new Vector3(0.9, 0.9, 0.9)
+                );
               }
             }
           });
@@ -214,14 +477,12 @@ export class mainScene {
             if (bbuttonComponent.pressed) {
               controller.getWorldPointerRayToRef(tmpRay3, false);
 
-              hit = this.scene.pickWithRay(tmpRay3);
+              const hit = this.scene.pickWithRay(tmpRay3);
 
               if (hit && hit.pickedMesh) {
-                if (hit.pickedMesh) {
-                  hit.pickedMesh.scaling.x *= 1.1;
-                  hit.pickedMesh.scaling.y *= 1.1;
-                  hit.pickedMesh.scaling.z *= 1.1;
-                }
+                hit.pickedMesh.scaling.multiplyInPlace(
+                  new Vector3(1.1, 1.1, 1.1)
+                );
               }
             }
           });
